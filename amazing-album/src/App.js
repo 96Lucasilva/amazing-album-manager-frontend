aae0bca85810fd4ee85e3d16282fb44672af09ea
Vinicius Souza <viniciusperuzza@yahoo.com>
2018-06-28 10:31:36 -0300

    Create Stylesheet for AlbumListing | Put AlbumListing inside a flex-box for alignment | Add 'capa' property for albums object

--- amazing-album/src/App.js
+++ amazing-album/src/App.js
@@ -6,8 +6,8 @@ import AlbumList from './albumListing/albumListing';
 class App extends Component {
   state = {
     albums: [
-      {title: 'The Best of Bill Medley', artist: 'Bill Medley', year: '1990', albumLength: '10', id: '01'},
-      {title: 'Frontiers', artist: 'Journey', year: '1983', albumLength: '14', id: '02'}
+      {title: 'The Best of Bill Medley', artist: 'Bill Medley', year: '1990', albumLength: '10', capa: 'https://cps-static.rovicorp.com/3/JPG_500/MI0002/471/MI0002471400.jpg?partner=allrovi.com', id: '01'},
+      {title: 'Frontiers', artist: 'Journey', year: '1983', albumLength: '14', capa: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f8/Jfrontiers.jpg/220px-Jfrontiers.jpg', id: '02'}
     ]
   };
 
@@ -16,6 +16,7 @@ class App extends Component {
       <div className="App">
         {this.state.albums.map((albums, index) => {
           return <AlbumList
+              capa={albums.capa}
               title={albums.title}
               artist={albums.artist}
               year={albums.year}
--- amazing-album/src/albumListing/albumListing.js
+++ amazing-album/src/albumListing/albumListing.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import './albumListing.css';
 
 const AlbumList = (props) => {
   return (
-    <div>
-      <img src="http://via.placeholder.com/50x50" />
-      <p>{props.title}</p>
+    <div className="albumListing">
+      <div className="albumListing-image">
+      <img src={props.capa} />
+    </div>
+    <div className="albumListing-text">
+      <p className="albumTitle">{props.title}</p>
       <p>Artist: {props.artist}</p>
       <p>Year: {props.year}</p>
       <p>Number of Tracks: {props.albumLength}</p>
+    </div>
     </div>
   )
 };
