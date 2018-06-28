a29ba7917d524ea9d2461bedfeab2eae68d6435d
Vinicius Souza <viniciusperuzza@yahoo.com>
2018-06-28 14:04:54 -0300

    Update CSS for App and AlbumListing Component

--- amazing-album/src/App.js
+++ amazing-album/src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   state = {
     albums: [
       {title: 'The Best of Bill Medley', artist: 'Bill Medley', year: '1990', albumLength: '10', capa: 'https://cps-static.rovicorp.com/3/JPG_500/MI0002/471/MI0002471400.jpg?partner=allrovi.com', id: '01'},
-      {title: 'Frontiers', artist: 'Journey', year: '1983', albumLength: '14', capa: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f8/Jfrontiers.jpg/220px-Jfrontiers.jpg', id: '02'}
+      {title: 'Frontiers', artist: 'Journey', year: '1983', albumLength: '14', capa: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f8/Jfrontiers.jpg/220px-Jfrontiers.jpg', id: '02'},
+      {title: 'Can\'t Hold Back', artist: 'Eddie Money', year: '1986', albumLength: '10', capa: 'https://images-na.ssl-images-amazon.com/images/I/512CqlcQAvL._SY355_.jpg', id: '03'}
     ]
   };
 
--- amazing-album/src/albumListing/albumListing.js
+++ amazing-album/src/albumListing/albumListing.js
@@ -6,13 +6,17 @@ const AlbumList = (props) => {
     <div className="albumListing">
       <div className="albumListing-image">
       <img src={props.capa} />
-    </div>
-    <div className="albumListing-text">
-      <p className="albumTitle">{props.title}</p>
-      <p>Artist: {props.artist}</p>
-      <p>Year: {props.year}</p>
-      <p>Number of Tracks: {props.albumLength}</p>
-    </div>
+      </div>
+      <div className="albumListing-text">
+        <div className="albumTitle">
+          <p>{props.title}</p>
+        </div>
+        <div className="albumListing-text">
+          <p>Artist: {props.artist}</p>
+          <p>Year: {props.year}</p>
+          <p>Number of Tracks: {props.albumLength}</p>
+        </div>
+      </div>
     </div>
   )
 };
